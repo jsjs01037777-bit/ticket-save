e449bd7a6d3dc5a2ce3d31ca855f52dc67a6e045
ticket-save <ticket-save@example.local>
2026-07-17 16:09:40 +0900

    Remove diagnostic API routes

--- api/ping.js
+++
@@ -1,5 +0,0 @@
-module.exports = function handler(req, res) {
-  res.setHeader("content-type", "application/json; charset=utf-8");
-  res.statusCode = 200;
-  res.end(JSON.stringify({ ok: true, method: req.method, query: req.query || {} }));
-};
